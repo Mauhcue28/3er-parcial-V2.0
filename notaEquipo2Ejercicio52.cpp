c0cb6a9cd7594abe80782857979e6f9fe9f5440f
MAURICIO CUECUECHA HERNANDEZ <168512051+Mauhcue28@users.noreply.github.com>
2024-06-10 10:18:32 -0600

    Update notaEquipo2Ejercicio52.cpp

--- notaEquipo2Ejercicio52.cpp
+++ notaEquipo2Ejercicio52.cpp
@@ -10,30 +10,31 @@ float calcularPromedio(float parcial1, float parcial2, float parcial3) {
    return (parcial1 * 0.2) + (parcial2 * 0.2) + (parcial3 * 0.6);
 }
 
-float obtenerNotaMaxima(float notaExamen, float notaContinua){ return(notaExamen > float notaContinua)? notaExamen : notaContinua;
+float obtenerNotaMaxima(float notaExamen, float notaContinua){ 
+return(notaExamen > notaContinua)? notaExamen : notaContinua;
 } //Mauricio_funcion_promedio
 
 //elizabeth:declarar y leer las calificaciones
 int main () {
   char respuesta;
 for(;;) {
-  float parcial1,parcial2,parcial3,notaexamen,notacontinua,notafinal;
+  float parcial1,parcial2,parcial3,notaExamen,notaContinua,notaFinal;
   cout<<"ingrese la calificacion del primer parcial";
   cin>>parcial1;
   cout<<"ingrese la calificacion del segundo parcial";
   cin>>parcial2;
   cout<<"ingrese la calificacion de la nota continua";
-  cin>>notacontinua;
+  cin>>notaContinua;
   //Alexandra Calcular promedio final
-   notaExamen=calularPromedio(parcial1,parcial2,parcial3);
-   notaFinal=obtenerNotaMaxima(notaExamen,notaContinua);
+   notaExamen= calcularPromedio(parcial1,parcial2,parcial3);
+   notaFinal= obtenerNotaMaxima(notaExamen,notaContinua);
 
    cout<<"La nota final es:"<<notaFinal<<endl;
 
    cout<<"Desea ingresar una nueva nota?(s para salir)";
-   cin>>respuesta;
+   cin>> respuesta;
 
-   while(respuesta=="s"){
+   while(respuesta=='s') {
        break;
    }
 }
